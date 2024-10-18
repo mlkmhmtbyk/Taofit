d839bb6e4c1b46e0e810b83b068dad9c921a36fa
mlkmhmtbyk <biyik.melikmehmet@gmail.com>
2024-10-18 10:27:37 +0300

    food için CRUD işlemleri

--- backend/controllers/food.controller.js
+++ backend/controllers/food.controller.js
@@ -1,4 +1,5 @@
 import Food from "../models/food.model.js";
+import mongoose from "mongoose";
 
 export const createFood = async (req, res) => {
   const food = req.body;
@@ -26,5 +27,58 @@ export const createFood = async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(400).json({ success: false, message: "Server error" });
+  }
+};
+
+export const getFoods = async (req, res) => {
+  try {
+    const foods = await Food.find();
+    res.status(200).json({ success: true, data: foods });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ success: false, message: "Server error" });
+  }
+};
+
+export const getFoodById = async (req, res) => {
+  try {
+    const food = await Food.findById(req.params.id);
+    res.status(200).json({ success: true, data: food });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ success: false, message: "Server error" });
+  }
+};
+
+export const updateFood = async (req, res) => {
+  const { id } = req.params;
+  const food = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ success: false, message: "Invalid food id" });
+  }
+
+  try {
+    const updatedFood = await Food.findByIdAndUpdate(id, food, { new: true });
+    res.status(200).json({ success: true, data: updatedFood });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
+
+export const deleteFood = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ success: false, message: "Invalid food id" });
+  }
+
+  try {
+    await Food.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Food deleted" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Server error" });
   }
 };
--- backend/routes/food.route.js
+++ backend/routes/food.route.js
@@ -1,8 +1,18 @@
 import express from "express";
-import {createFood} from "../controllers/food.controller.js";
+import {
+  createFood,
+  getFoods,
+  getFoodById,
+  updateFood,
+  deleteFood,
+} from "../controllers/food.controller.js";
 
 const router = express.Router();
 
 router.post("/", createFood);
+router.get("/:id", getFoodById);
+router.get("/", getFoods);
+router.put("/:id", updateFood);
+router.delete("/:id", deleteFood);
 
-export default router; 
+export default router;
