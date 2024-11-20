4ee95784ba256312ff688895699c22c17b4b4cd3
mlkmhmtbyk <biyik.melikmehmet@gmail.com>
2024-11-20 17:44:17 +0300

    Authorization ve cookie parser yapıldı

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import { connectDB } from "./config/db.js";
 import foodRoutes from "./routes/food.route.js";
 import mealRoutes from "./routes/meal.route.js";
@@ -12,6 +13,8 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.use("/api/foods", foodRoutes);
 app.use("/api/meals", mealRoutes);
